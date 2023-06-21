2b71d29c62b904be1027c83290f3ac2a3a830447
andrii <quadr988@gmail.com>
2023-06-21 21:20:50 +0300

    init

--- src/main/java/com/andmal/HelloGrpcService.java
+++ src/main/java/com/andmal/HelloGrpcService.java
@@ -1,14 +1,30 @@
 package com.andmal;
 
+import com.andmal.mono.MonoTransaction;
 import io.quarkus.grpc.GrpcService;
 
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 @GrpcService
 public class HelloGrpcService implements TransactionsGrpc {
-
     @Override
     public Uni<TransReply> getTransaction(TransRequest request) {
-        return Uni.createFrom().item(TransReply.newBuilder().build());
+        return MonoTransaction.findByIdCustom(request.getId()).map(tr -> {
+           return TransReply.newBuilder()
+                    .setId(tr.id)
+                    .setAccount(tr.account)
+                    .setAmount(tr.amount)
+                    .setCurrency(tr.currency)
+                    .setType(tr.type)
+                    .setWhen(tr.when.toString())
+                    .setMoment(tr.moment.toString())
+                    .build();
+        });
+    }
+
+    @Override
+    public Multi<TransReply> getTransactions(TransRequest request) {
+        return null;
     }
 }
--- src/main/java/com/andmal/rest/TransResource.java
+++ src/main/java/com/andmal/rest/TransResource.java
@@ -1,18 +1,15 @@
 package com.andmal.rest;
 
 import com.andmal.mono.MonoTransaction;
-import com.andmal.mono.MonoUser;
 import com.andmal.mono.dto.MonoTransactionDto;
 import com.andmal.repo.MonoUserRepo;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
-import jakarta.persistence.PersistenceUnit;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Path("/transactions")
 public class TransResource {
@@ -20,16 +17,20 @@ public class TransResource {
 //    @PersistenceUnit("")
     MonoUserRepo userRepo;
 
+    @WithSession
     @GET
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<MonoTransactionDto> getTransactions(@QueryParam("limit") int limit) {
-        return MonoTransaction.getAll().await().indefinitely().stream().map(tre -> {
-            MonoTransactionDto dto = new MonoTransactionDto();
-            dto.account = tre.account;
-            dto.amount = tre.amount;
-            return dto;
-        }).collect(Collectors.toList());
+    public Uni<List<MonoTransactionDto>> getTransactions(@QueryParam("limit") int limit) {
+        return MonoTransaction.getAll().map(trList -> {
+            var dtoList = trList.stream().map(tre -> {
+                MonoTransactionDto dto = new MonoTransactionDto();
+                dto.account = tre.account;
+                dto.amount = tre.amount;
+                return dto;
+            }).toList().subList(0, limit);
+            return dtoList;
+        });
     }
 
     @WithSession
@@ -47,7 +48,6 @@ public class TransResource {
             dto.when = tre.when;
             return dto;
         });
-//        return userRepo.findById(id).map(t -> t.getName());
     }
 
 
